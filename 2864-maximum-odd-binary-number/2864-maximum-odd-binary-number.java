7895be3d315348d034fa5d84467665dde17e71eb
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2024-03-08 18:39:51 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (34.15%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,19 +1,41 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
         
-        char[] str = s.toCharArray();
+//         char[] str = s.toCharArray();
         
-        Arrays.sort(str);
+//         Arrays.sort(str);
+        
+//         int n = str.length;
+        
+//         int last = n-2;
+        
+//         for(int i =0;i<n/2;i++){
+//             char temp = str[i];
+//             str[i] = str[last -i];
+//             str[last-i] = temp;
+//         }
+        
+//         return new String(str);
+        
+    char[] str = s.toCharArray();
         
         int n = str.length;
         
-        int last = n-2;
+        int left =0, right = n-1;
         
-        for(int i =0;i<n/2;i++){
-            char temp = str[i];
-            str[i] = str[last -i];
-            str[last-i] = temp;
+        while(left <=right){
+            if(str[left] == '1')
+                left++;
+            if(str[right] == '0')
+                right--;
+            if(left<=right && str[left] == '0' && str[right] == '1'){
+                char temp =str[left];
+                str[left]= str[right];
+                str[right] = temp;
+            }
         }
+        str[left-1] = '0';
+        str[n-1] ='1';
         
         return new String(str);
         
