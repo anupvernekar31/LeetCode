61a891c48348c5a9d61deeaca95c83cc97138620
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2024-08-26 12:38:46 +0530

    Time: 51 ms (35.54%), Space: 44.1 MB (55.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,4 +1,27 @@
 class Solution {
+//     public int lengthOfLIS(int[] nums) {
+        
+//         int n = nums.length;
+        
+//         // int ans[] = new int[n];
+        
+        
+//         int dp[]=new int[n];
+//         Arrays.fill(dp, 1);
+//         int maxi =1;
+//         for(int ind=0;ind<n;ind++){
+//             for(int prev=0;prev<ind;prev++){
+//                 if(nums[prev] < nums[ind]){
+//                     dp[ind] = Math.max(dp[ind], 1+ dp[prev]);
+//                 }
+//             }
+//             maxi = Math.max(maxi, dp[ind]);
+//         }
+        
+//         return maxi;
+        
+//     }
+    
     public int lengthOfLIS(int[] nums) {
         
         int n = nums.length;
@@ -7,16 +30,35 @@ class Solution {
         
         
         int dp[]=new int[n];
+        int hash[] = new int[n];
+        for(int i=0;i<n;i++){
+            hash[i] = i;
+        }
         Arrays.fill(dp, 1);
         int maxi =1;
+        int last_ind = 0;
         for(int ind=0;ind<n;ind++){
             for(int prev=0;prev<ind;prev++){
-                if(nums[prev] < nums[ind]){
+                if((nums[prev] < nums[ind]) && dp[prev] + 1 > dp [ind] ){
                     dp[ind] = Math.max(dp[ind], 1+ dp[prev]);
+                    hash[ind] = prev;
                 }
             }
-            maxi = Math.max(maxi, dp[ind]);
+            if(dp[ind] > maxi){
+                maxi = dp[ind];
+                last_ind = ind;
+            }
         }
+        
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        ans.add(nums[last_ind]);
+        
+        while(last_ind != hash[last_ind]){
+            last_ind = hash[last_ind];
+            ans.add(nums[last_ind]);
+        }
+        
+        System.out.println(ans);
         
         return maxi;
         
