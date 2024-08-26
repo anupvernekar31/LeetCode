ebda47a782026450ad0d647862d400a53e07183a
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2024-08-26 13:18:53 +0530

    Time: 115 ms (20.62%), Space: 45.3 MB (23.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -22,45 +22,84 @@ class Solution {
         
 //     }
     
+//     public int lengthOfLIS(int[] nums) {
+        
+//         int n = nums.length;
+        
+//         // int ans[] = new int[n];
+        
+        
+//         int dp[]=new int[n];
+//         int hash[] = new int[n];
+//         for(int i=0;i<n;i++){
+//             hash[i] = i;
+//         }
+//         Arrays.fill(dp, 1);
+//         int maxi =1;
+//         int last_ind = 0;
+//         for(int ind=0;ind<n;ind++){
+//             for(int prev=0;prev<ind;prev++){
+//                 if((nums[prev] < nums[ind]) && dp[prev] + 1 > dp [ind] ){
+//                     dp[ind] = Math.max(dp[ind], 1+ dp[prev]);
+//                     hash[ind] = prev;
+//                 }
+//             }
+//             if(dp[ind] > maxi){
+//                 maxi = dp[ind];
+//                 last_ind = ind;
+//             }
+//         }
+        
+//         ArrayList<Integer> ans = new ArrayList<Integer>();
+//         ans.add(nums[last_ind]);
+        
+//         while(last_ind != hash[last_ind]){
+//             last_ind = hash[last_ind];
+//             ans.add(nums[last_ind]);
+//         }
+        
+//         System.out.println(ans);
+        
+//         return maxi;
+        
+//     }
+    
+    public int lower_bound(ArrayList<Integer> arrayList, int key) {
+        int low = 0, high = arrayList.size();
+        int mid;
+
+        while (low < high) {
+            mid = low + (high - low) / 2;
+
+            if (arrayList.get(mid) < key) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
+    
     public int lengthOfLIS(int[] nums) {
         
         int n = nums.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(nums[0]);
         
-        // int ans[] = new int[n];
-        
-        
-        int dp[]=new int[n];
-        int hash[] = new int[n];
-        for(int i=0;i<n;i++){
-            hash[i] = i;
-        }
-        Arrays.fill(dp, 1);
-        int maxi =1;
-        int last_ind = 0;
-        for(int ind=0;ind<n;ind++){
-            for(int prev=0;prev<ind;prev++){
-                if((nums[prev] < nums[ind]) && dp[prev] + 1 > dp [ind] ){
-                    dp[ind] = Math.max(dp[ind], 1+ dp[prev]);
-                    hash[ind] = prev;
-                }
-            }
-            if(dp[ind] > maxi){
-                maxi = dp[ind];
-                last_ind = ind;
+        for(int i=1;i<n;i++){
+            if(nums[i] > ans.get(ans.size()-1)){
+                ans.add(nums[i]);
+            } else {
+                int ind = lower_bound(ans, nums[i]);
+                ans.set(ind, nums[i]);
+                System.out.println(ans);
             }
         }
+        return ans.size();
         
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        ans.add(nums[last_ind]);
         
-        while(last_ind != hash[last_ind]){
-            last_ind = hash[last_ind];
-            ans.add(nums[last_ind]);
-        }
         
-        System.out.println(ans);
-        
-        return maxi;
         
     }
 }
