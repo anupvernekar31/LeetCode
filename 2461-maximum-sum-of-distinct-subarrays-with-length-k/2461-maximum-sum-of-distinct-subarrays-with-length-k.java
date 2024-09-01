e6c7fe2a55f65bd3c5157e6bfd66587131a4f3c4
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2024-09-01 16:45:34 +0530

    Time: 45 ms (56.92%), Space: 58.8 MB (46.87%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,23 +1,33 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
         
-//         int res = 0;
-        
-//         if(k>nums.length){
-//             return 0;
+        // ================================
+        // This code is valid if repititions of numbers are allowed and distinct element doesnt exist
+        // ================================
+//         int left = 0, right = 0;
+//         // variable to hold the answer
+//         int ans = Integer.MIN_VALUE;
+//         // sum to hold the sum of current on-going window
+//         int sum = 0;
+
+//         // while we have not reached till the end
+//         while (right < arr.length) {
+//             // add current right value to sum
+//             sum += arr[right];
+//             // if not reached the size of sliding window
+//             // just increase the right side
+//             if (right - left + 1 < k) {
+//                 right++;
+//             } else if (right - left + 1 == k) { // once we have reached the size of sliding window, we need to increase both the variables, but need addition logic
+//                 // first find the answer till now to hold the maxSum so far for the sub-arrays
+//                 ans = Math.max(ans, sum);
+//                 // as we are going to slide, so we reduce the sum of the left most element from sum variable
+//                 sum -= arr[left];
+//                 // finally increase both the pointers to slide the window
+//                 left++;
+//                 right++;
+//             }
 //         }
-        
-//         for(int i=0;i<k;i++){
-//             res+=nums[i];
-//         }
-//         int cursum = res;
-        
-//         for(int i=k;i<nums.length;i++){
-//             cursum += nums[i] - nums[i-k];
-//             res = Math.max(res, cursum);
-            
-//         }
-//         return res;
         
         HashMap<Integer ,Integer> mp= new HashMap<>();
         int i=0,j=0;
@@ -34,9 +44,9 @@ class Solution {
                 {
                     ans=Math.max(ans,sum);
                 }
-                    if(mp.get(nums[i]) ==1)
+                if(mp.get(nums[i]) ==1)
                     mp.remove(nums[i]);
-                    else
+                else
                     mp.put(nums[i],mp.get(nums[i])-1);
                 sum=sum-nums[i];
                 i++;
